db5e4465f1763dff0a8035d9b0b7fd28cdd827bb
madsterest <odea.madeleine@gmail.com>
2021-03-28 15:27:26 +1100

    Started working on the Javascript logic> Adding and removing classes depending on the time

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+var hours = document.querySelectorAll(".hour");
 
+var currentTime = moment().format("h");
+var textArea = document.querySelectorAll("textarea");
+console.log(textArea);
 
+for (var i = 0; i < hours.length; i++) {
+  var hoursText = hours[i].textContent;
+  var textBox = textArea[i];
+  if (currentTime === hoursText) {
+    textBox.classList.remove("future");
+    textBox.classList.add("present");
+  } else if (currentTime < hoursText) {
+    textBox.classList.remove("future");
+    textBox.classList.add("past");
+  }
+}
